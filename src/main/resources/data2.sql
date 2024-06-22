91a33f27b03cd6f83f9dc2252916a2fe4c96fcde
Glor1ous <pushin.illya@yandex.ru>
2024-06-22 13:50:59 +0300

    first commit

---
+++ src/main/resources/data2.sql
@@ -0,0 +1,13 @@
+insert into sample_person(version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1, 'Eula', 'Lane', 'eula.lane@jigrormo.ye', '(762) 526-5961', '1954-10-29', 'Insurance Clerk');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 2, 'Ilya', 'Pushin', 'pushin.illia@yandex.ru', '89892640862', '2024-06-13', 'Python');
+
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1001, 'Илья', 'Пушин', 'pushin.illia@yandex.ru', '89892640862', '2024-06-12', 'Python');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1002, 'Илья', 'Пушин', 'pushin.illia@yandex.ru', '89892640862', '2024-06-28', 'Godot Engine');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1003, 'Ilia', 'Pushin', 'pusinila9@gmail.com', '89892640862', '2024-07-05', 'Unreal Engine');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1004, 'Ilia', 'Pushin', 'pusinila9@gmail.com', '89892640862', '2024-06-20', 'Python');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1005, 'Вова', 'Пушинян', 'vova@gmail.com', '89892640862', '2024-09-04', 'JavaScript');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1006, 'Вова', 'Пушинян', 'vova@gmail.com', '89892640862', '2024-09-04', 'JavaScript');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1007, 'Иван', 'Пушинян', 'vova@gmail.com', '89892640862', '2024-09-04', 'JavaScript');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1008, 'Иван', 'Пушинян', 'vova@gmail.com', '89892640862', '2024-09-04', 'JavaScript');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1009, 'Иван', 'Пушинян', 'vova@gmail.com', '89892640862', '2024-09-04', 'JavaScript');
+insert into sample_person (version, id, first_name, last_name, email, phone, date_of_birth, course) values (1, 1010, 'Ilia', 'Pushin', 'pusinila9@gmail.com', '89892640862', '2024-06-27', 'Unreal Engine');
